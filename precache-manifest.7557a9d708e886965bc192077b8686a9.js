f0fbbc5c6c8d3a959c8d4389a1496d2868d61348
ventada <m.ghahri2025@gmail.com>
2019-02-07 21:02:27 +0330

    Updates

---
+++ precache-manifest.7557a9d708e886965bc192077b8686a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "a02db00baf6ed2ad3455",
+    "url": "/contactmanager/static/js/main.a02db00b.chunk.js"
+  },
+  {
+    "revision": "9a8f2f8aa5edc5c39f0f",
+    "url": "/contactmanager/static/js/1.9a8f2f8a.chunk.js"
+  },
+  {
+    "revision": "9a8f2f8aa5edc5c39f0f",
+    "url": "/contactmanager/static/css/1.69f874c0.chunk.css"
+  },
+  {
+    "revision": "3496f5cd52656080d8f09a3f5e0e97b7",
+    "url": "/contactmanager/index.html"
+  }
+];
